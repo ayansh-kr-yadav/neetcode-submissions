500cdcfdee32c68a9ef8d799ff07a8cff839e930
Ayansh Kr Yadav <chunnukumar013@gmail.com>
2026-08-16 13:50:17 +0530

    Add: length-of-last-word - submission-1

---
+++ Data Structures & Algorithms/length-of-last-word/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        // traverse the string from back  
+        // if any space found in the string trverse will break / exit from loop 
+        // and return store count;
+        int count=0 ;
+        int end= s.length()-1;
+        while(end>=0){
+            if(s[end]==' ' && count > 0){
+             return count ;
+            } else if (s[end]==' ') {
+            end--;
+            } else
+            {
+                count ++;
+                end--;
+            }
+
+            // if(s[end]==' ' && count > 0){
+            //     return count;
+            // }
+        }
+        return count;
+
+
+    }
+};
