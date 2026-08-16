601d43a406a25e3ffb5a6c57aaf0debda2629075
Ayansh Kr Yadav <chunnukumar013@gmail.com>
2026-08-16 09:27:01 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        // traverse the substring and store this into the hashmap
+        // in hashmap the value of key will be the index of string /key
+         // travere the string and now compare the index value 
+         // if index value of that is key is less than the current index
+         // return false
+         if( t.length()==0){
+            return false;
+         }
+            unordered_map<char, int> hash;
+
+         for( int i= 0 ; i< t.length() ; i++){
+            hash[t[i]]=i;
+         }
+
+         for(int i= 0 ; i< s.length() ; i++){
+            if(hash[s[i]]< i){
+                return false;
+            }
+         }
+         
+         return true;
+    }
+};
